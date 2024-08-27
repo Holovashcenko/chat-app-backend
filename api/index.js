3ef6027f1fcfbbac2ad238c36b5168fa096022be
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2024-08-27 14:28:05 +0300

    testing end

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const connectDB = require('./config/db')
 const routes = require('./routes')
 const initializeChats = require('./config/initChats')
-// const sendRandomMessage = require('./config/randomMessageSender')
+const sendRandomMessage = require('./config/randomMessageSender')
 const { initSocket } = require('./config/socket')
 
 const app = express()
@@ -30,9 +30,9 @@ const server = http.createServer(app)
 
 initSocket(server)
 
-// setInterval(() => {
-//   sendRandomMessage()
-// }, 180000)
+setInterval(() => {
+  sendRandomMessage()
+}, 180000)
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
